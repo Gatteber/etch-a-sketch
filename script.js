ea85316f4271f618a584ccb04943884d72c43797
Gatteber <Garrett.Atteberry@gmail.com>
2022-06-17 10:54:26 +0900

    modified tutorial a bit to match box style. display working.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+//slider variables
+var slider = document.getElementById("etchRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value + " x " + slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value + " x " + this.value;
+}
